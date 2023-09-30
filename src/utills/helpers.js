5bc1c51702df1029ed6446ea8aca416613ed8e41
devn5900 <devn5900@gmail.com>
2023-09-30 14:42:17 +0530

    scrolling sync, category panel, cart functionality, responsiveness :DONE

--- src/utills/helpers.js
+++ src/utills/helpers.js
@@ -13,14 +13,14 @@ export const  handleScroll=(headings)=> {
         headings.forEach((head,j)=>{
           head.classList.remove('lg:fixed');
           head.classList.remove('lg:z-50');
-          head.classList.remove('lg:top-[15%]');
+          head.classList.remove('lg:top-[14%]');
         heading.classList.add('lg:opacity:0');
         head.classList.remove('lg:py-2');
         heading.nextSibling.classList.remove("pt-10")
 
     })
         heading.classList.add('lg:fixed');
-        heading.classList.add('lg:top-[15%]');
+        heading.classList.add('lg:top-[14%]');
         heading.classList.add('lg:z-50');
         heading.classList.remove('lg:opacity:0');
         heading.classList.add('lg:py-2');
@@ -29,7 +29,7 @@ export const  handleScroll=(headings)=> {
         heading.classList.remove('lg:fixed');
         heading.classList.remove('lg:z-50');
         heading.classList.add('lg:opacity:0');
-        heading.classList.remove('lg:top-[15%]');
+        heading.classList.remove('lg:top-[14%]');
         heading.classList.remove('lg:py-2');
         heading.nextSibling.classList.remove("pt-10")
 
